b9ef4ee859be79e36dca257ac89542021c36667e
Bogdan <brendan228008@gmail.com>
2024-12-16 00:52:34 +0300

    123

--- type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"ImageProcessor"},{"p":"<Unnamed>","l":"SteganographyApp"}];updateSearchResults();
